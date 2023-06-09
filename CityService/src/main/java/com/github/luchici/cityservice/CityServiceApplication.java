b3d2692511954de9a909390863cb83a6ed69210b
DESKTOP-F1ROUTL\root1 <luchici123@gmail.com>
2023-06-09 14:28:28 +0300

    Initial commit. Adding parent pom.xml, Frontend-Thymeleaf module and CityService module

---
+++ CityService/src/main/java/com/github/luchici/cityservice/CityServiceApplication.java
@@ -0,0 +1,11 @@
+package com.github.luchici.cityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CityServiceApplication.class, args);
+    }
+}
---
+++ Frontend-Thymeleaf/src/main/java/com/github/luchici/thymeleaf/SmartcityThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.github.luchici.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcityThymeleafApplication.class, args);
+	}
+}
