0759260910bb0147c5dd6f6f688f7b184600f74a
DESKTOP-F1ROUTL\root1 <luchici123@gmail.com>
2023-06-11 20:41:49 +0300

    Initial commit AuthService

---
+++ AuthService/src/main/java/com/github/luchici/authservice/AuthController.java
@@ -0,0 +1,9 @@
+package com.github.luchici.authservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+
+}
---
+++ AuthService/src/test/java/com/github/luchici/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.luchici.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads(WebApplicationContext context) {
+		assertNotNull(context);
+	}
+
+}
